dc16f50c7db8cfaade5a36a25c4f756e58139315
Nikita Chupahin <nik123chupahin@gmail.com>
2025-09-15 14:11:40 +0300

    updated task solution

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,9 @@
 import classNames from 'classnames';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav
       data-cy="nav"
@@ -14,22 +16,22 @@ export const Navbar = () => {
           <NavLink
             to="/"
             end
-            className={({ isActive }) => {
-              return classNames('navbar-item', {
+            className={({ isActive }) =>
+              classNames('navbar-item', {
                 'has-background-grey-lighter': isActive,
-              });
-            }}
+              })
+            }
           >
             Home
           </NavLink>
 
           <NavLink
-            to="/people"
-            className={({ isActive }) => {
-              return classNames('navbar-item', {
+            to={{ pathname: '/people', search: location.search }}
+            className={({ isActive }) =>
+              classNames('navbar-item', {
                 'has-background-grey-lighter': isActive,
-              });
-            }}
+              })
+            }
           >
             People
           </NavLink>
--- src/components/PersonLink/PersonLink.tsx
+++ src/components/PersonLink/PersonLink.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Person } from '../../types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 
 interface Props {
@@ -9,13 +9,18 @@ interface Props {
 }
 
 export const PersonLink: React.FC<Props> = ({ person, name }) => {
+  const location = useLocation();
+
   if (!person) {
     return <span>{name}</span>;
   }
 
   return (
     <Link
-      to={`/people/${person.slug}`}
+      to={{
+        pathname: `/people/${person?.slug || ''}`,
+        search: location.search,
+      }}
       className={classNames({
         'has-text-danger': person.sex === 'f',
       })}
